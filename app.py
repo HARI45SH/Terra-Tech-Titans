7d97e4c3e915cf9a6a934689c1795ce39b5e54e8
HARI45SH <21204048@rmd.ac.in>
2023-09-18 19:06:47 +0530

    commit

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
-from flask import Flask, render_template, request, jsonify
-from torchvision import transforms
-from PIL import Image
-import torch
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-# Load your Deep Learning model
-model = torch.load('model.pth')
-model.eval()  # Set the model to evaluation mode
-
-# Define a transformation to preprocess the image
-
-
-# Define a function to preprocess the image
+# Set the upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    image_file = request.files['file']
-    if image_file:
-        image = Image.open(image_file)
-        input_image = preprocess_image(image)
-        
-        with torch.no_grad():
-            output = model(input_image)
-        
-        # Process the output based on your model's architecture
-        # For example, if it's a classification model, you might do:
-        _, predicted_class = torch.max(output, 1)
-        class_names = ['asphalt', 'bricks']  # Replace with your class names
-        prediction = class_names[predicted_class]
-        
-        return jsonify({'result': prediction})
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+
+    if file:
+        # Save the file
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        return "File uploaded successfully"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
