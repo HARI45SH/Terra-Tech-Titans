fb500be94afa890700391a03f01740c21f756d05
manikandan793 <mm5958753@gmail.com>
2023-09-21 15:53:29 +0530

    commit

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request
+import torch
+from PIL import Image
 import os
+import numpy as np
+from torchvision import transforms
 
 app = Flask(__name__)
 
 # Set the upload folder
 UPLOAD_FOLDER = 'uploads'
+if os.path.isdir(UPLOAD_FOLDER) == False:
+    os.mkdir(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+
+from python_files.model_eval import Load_model
+model=Load_model(Path=None)
+
+app.config['model']=model
 
 @app.route('/')
 def index():
@@ -26,7 +38,13 @@ def upload_file():
         # Save the file
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
+        tensor=torch.from_numpy(np.array(Image.open(filename).resize((224,224)))).permute(2,0,1).unsqueeze(0)
+        tensor=transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])(tensor.float())
+        cl,slip,rough=model(tensor)
         return "File uploaded successfully"
+    return jsonify({'Slipperiness': str(slip) , 'Roughness': str(rough)})
+
+
 
 
 if __name__ == '__main__':
