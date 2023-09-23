1892929e74ca80455fc168dfe5309c5dca0cf811
HARI45SH <21204048@rmd.ac.in>
2023-09-24 00:31:05 +0530

    commit

--- app.py
+++ app.py
@@ -1,87 +1,48 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 import torch
+from PIL import Image
 import os
-import torch.nn as nn
-from torchvision import models
-import torch.nn.functional as F
 import numpy as np
-
+from io import BytesIO
+from torchvision import transforms
 
 app = Flask(__name__)
 
+# Set the upload folder
 UPLOAD_FOLDER = 'uploads'
+if os.path.isdir(UPLOAD_FOLDER) == False:
+    os.mkdir(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+from python_files.model_eval import Load_model
+model=Load_model(Path=None)
 
-class Mobilenet_reg(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.mobilenet_model=models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1)
-        self.mobilenet_model.classifier[3]=nn.Identity()
-        self.classifier=nn.Linear(1024,31)
-        self.regression=nn.Sequential(
-            nn.Linear(1024,256),
-            nn.GELU(),
-            nn.Linear(256,2)
-        )
+app.config['model']=model
 
-    def forward(self,x):
-        extraction=self.mobilenet_model(x)
-        class_pred=self.classifier(extraction)
-        regression_pred=self.regression(extraction)
-
-        return class_pred,regression_pred
-
-
-class Load_model():
-    def __init__(self,Path):
-        self.model=Mobilenet_reg()
-        self.PATH = Path
-        if self.PATH!=None:
-            self.model.load_state_dict(self.PATH)
-    
-    def __call__(self,image):
-        class_pred,slip_rough=self.model(image)
-        slip,rough=slip_rough[0][0].item(),slip_rough[0][1].item()
-
-        return class_pred.argmax(dim=1).item(),slip,rough
-
-
-@app.route('/', methods=['GET'])
-def hello_word():
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/upload',methods=['POST'])
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+
     file = request.files['file']
     if file.filename == '':
         return "No selected file"
 
-    filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    image_path = "uploads/" + file.filename
-    file.save(filename)
-    
-
-    
-
-
-
-
-    return jsonify({'Slipperiness': regression_pred , 'Roughness': slip_rough})
-
-
-
-
-
-
-
-
-
+    if file:
+        # Save the file
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        tensor=torch.from_numpy(np.array(Image.open(filename).resize((224,224)))).permute(2,0,1).unsqueeze(0)
+        tensor=transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])(tensor.float())
+        cl,slip,rough=model(tensor)
+        
+        return jsonify({'predictions': cl, 'Roughness': str(round(rough,2)), 'Slipperiness': str(round(slip,2))})
 
 
 if __name__ == '__main__':
-    m=Load_model(Path=None)
-    print(m(
-        torch.randn(1,3,224,224)
-    ))
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
